baa20b40e231aa64e9a65554731850c3dfa7f7ca
Diego Bispo <dbispo@gmail.com>
2020-10-21 09:37:47 -0300

    Initial commit

---
+++ NVRCsharpDemo/Listener/HttpBodyParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NVRCsharpDemo.Listener
+{
+    public class HttpBodyParameters
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Ip { get; set; }
+        public int Port { get; set; }
+        public int Camera { get; set; }
+        public DateTime Date { get; set; }
+        public string Device { get; set; }
+    }
+}
---
+++ NVRCsharpDemo/Listener/HttpResult.cs
@@ -0,0 +1,14 @@
+﻿namespace NVRCsharpDemo.Listener
+{
+    public class HttpResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public HttpResult(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
